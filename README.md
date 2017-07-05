0d9ca024b4881c2ba26d8cdb3a3a8d257b5330f1
xxzl0130 <zwx0130@buaa.edu.cn>
2017-07-05 15:15:26 +0800

    Add example and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# CS5460
+Arduino library for CS5460
+
+
+Using SPI interface to connct, tested on Arduino UNO.
+
+Current functions include set config register arguments manually, read measurements in raw binary format and float number -1.0~1.0.
+
+Todo:
+Add functions to calculate true measurements and make full use of EOUT, EDIR and INT ports.
---
+++ examples/CS5460_simple/CS5460_simple.ino
@@ -0,0 +1,35 @@
+#include <CS5460.h>
+
+/*
+ * for UNO
+ * MOSI 11 --- 23
+ * MISO 12 --- 6
+ * SCLK 13 --- 5
+ * SS   10 --- 7
+ */
+CS5406 powerMeter(10);
+
+void setup()
+{
+    Serial.begin(9600);
+    powerMeter.init();
+    powerMeter.startMultiConvert();
+}
+
+void loop()
+{
+    Serial.print(powerMeter.getCurrent());
+    Serial.print("\t");
+    Serial.print(powerMeter.getVoltage());
+    Serial.print("\t");
+    Serial.print(powerMeter.getPower());
+    Serial.print("\n");
+    Serial.print(powerMeter.getRawCurrent(),HEX);
+    Serial.print("\t");
+    Serial.print(powerMeter.getRawVoltage(),HEX);
+    Serial.print("\t");
+    Serial.print(powerMeter.getRawPower(),HEX);
+    Serial.print("\n");
+    delay(500);
+}
+
